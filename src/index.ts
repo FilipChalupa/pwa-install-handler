39a5c7a1cb9b085b2d517a5e69d050fa59c179c1
Filip Chalupa <chalupa.filip@gmail.com>
2019-02-11 11:25:29 +0100

    Prototype

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello");
+type CanInstallCallback = (canInstall: boolean) => void
+type InstallEvent = Event | null
+
+class Install {
+	private event: InstallEvent = null
+	private callbacks: CanInstallCallback[] = []
+
+	constructor() {
+		window.addEventListener('beforeinstallprompt', (event) => {
+			event.preventDefault()
+			this.updateEvent(event)
+		})
+	}
+
+	public prompt() {
+		// @ts-ignore
+		this.event.prompt()
+		// @ts-ignore
+		this.event.userChoice.then(() => {
+			this.updateEvent(null)
+		})
+	}
+
+	private updateEvent(event: InstallEvent) {
+		this.event = event
+		this.callbacks.forEach((callback) => callback(Boolean(event)))
+	}
+
+	public addListener(callback: CanInstallCallback) {
+		callback(Boolean(this.event))
+		this.callbacks.push(callback)
+	}
+}
+
+const install = new Install()
+
+export default install
