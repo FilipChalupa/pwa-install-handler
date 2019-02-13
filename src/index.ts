2d50f3ece7e4800f1a3b2c36357c40e909e42489
Filip Chalupa <chalupa.filip@gmail.com>
2019-02-13 23:37:27 +0100

    Listeners: remove fix

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,7 @@ class PwaInstallHandler {
 	}
 
 	public removeListener(callback: CanInstallCallback): void {
-		this.callbacks.filter((cb) => callback !== cb)
+		this.callbacks = this.callbacks.filter((cb) => callback !== cb)
 	}
 }
 
