c7cc8a252543f97ab84e79a31d18db70fe963d6b
Filip Chalupa <chalupa.filip@gmail.com>
2020-08-31 23:17:35 +0200

    SSR: type check fix

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ class PwaInstallHandler {
 	private callbacks: CanInstallCallback[] = []
 
 	constructor() {
-		if (typeof window === undefined) {
+		if (typeof window === 'undefined') {
 			return
 		}
 
