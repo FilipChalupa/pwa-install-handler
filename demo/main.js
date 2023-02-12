343e320bacebcbb50f783390484de581bc54ea38
Filip Chalupa <chalupa.filip@gmail.com>
2023-02-12 22:12:39 +0100

    Add demo

---
+++ demo/main.js
@@ -0,0 +1,15 @@
+import { pwaInstallHandler } from 'https://unpkg.com/pwa-install-handler@latest'
+
+const $button = document.querySelector('#installButton')
+
+pwaInstallHandler.addListener((canInstall) => {
+	$button.style.display = canInstall ? 'inline-block' : 'none'
+})
+
+$button.addEventListener('click', () => {
+	pwaInstallHandler.install()
+})
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('service-worker.js')
+}
