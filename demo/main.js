e2241b8c8b27a56fe3e8857d7c23a1589ef212eb
Filip Chalupa <chalupa.filip@gmail.com>
2024-12-10 14:02:19 +0100

    Demo: add logging

--- demo/main.js
+++ demo/main.js
@@ -1,15 +1,32 @@
 import { pwaInstallHandler } from 'https://unpkg.com/pwa-install-handler@latest?module'
 
 const $button = document.querySelector('#installButton')
+const $logOutput = document.querySelector('#logOutput ul')
+
+const log = (message) => {
+	const $logItem = document.createElement('li')
+	const now = new Date()
+	$logItem.textContent = `[${now.toLocaleTimeString()}.${now.getMilliseconds()}] ${message}`
+	$logOutput.prepend($logItem)
+}
 
 pwaInstallHandler.addListener((canInstall) => {
-	$button.style.display = canInstall ? 'inline-block' : 'none'
+	$button.style.visibility = canInstall ? 'inherit' : 'hidden'
+	log(canInstall ? '✔️ App can be installed.' : '⚠️ App cannot be installed.')
 })
 
-$button.addEventListener('click', () => {
-	pwaInstallHandler.install()
+$button.addEventListener('click', async () => {
+	const result = await pwaInstallHandler.install()
+	log(result ? '✔️ App installed.' : '❌ App not installed.')
 })
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('service-worker.js')
+	navigator.serviceWorker
+		.register('service-worker.js')
+		.then(() => {
+			log('✔️ Service worker registered.')
+		})
+		.catch(() => {
+			log('❌ Service worker registration failed.')
+		})
 }
